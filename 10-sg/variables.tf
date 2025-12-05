129a7c10f52f605d519aff8a73b2de7493905601
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-12-05 19:29:14 +0530

    sg and rules

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -1,47 +1,14 @@
 variable "project" {
-    default = "roboshop"  
+  default = "roboshop"  
 }
 
 variable "environment" {
-    default = "dev" 
+  default = "dev" 
 }
 
 variable "sg_tags"{
-    type = map(string)
-    default = {}
-}
-
-variable "frontend_sg_name" {
-  default = "frontend"
-}
-
-variable "frontend_sg_description" {
-    default = "this security group is created for frontend"  
-}
-
-
-variable "bastion_sg_name" {
-  default = "bastion"
-}
-
-variable "bastion_sg_description" {
-    default = "this security group is created for bastion"  
-}
-
-variable "backend-alb_sg_name" {
-  default = "backend-alb"
-}
-
-variable "backend-alb_sg_description" {
-    default = "this security group is created for backend-alb"  
-}
-
-variable "vpn_sg_name" {
-  default = "vpn"
-}
-
-variable "vpn_sg_description" {
-    default = "this security group is created for vpn"  
+  type = map(string)
+  default = {}
 }
 
 variable "mongodb_sg_name" {
@@ -49,10 +16,14 @@ variable "mongodb_sg_name" {
 }
 
 variable "mongodb_sg_description" {
-    default = "this security group is created for mongodb"  
+  default = "this security group is created for mongodb"  
 }
 
 variable "mongodb_vpn_ports" {
+  default = [22, 27017]  
+}
+
+variable "mongodb_bastion_ports" {
   default = [22, 27017]  
 }
 
@@ -61,11 +32,15 @@ variable "redis_sg_name" {
 }
 
 variable "redis_sg_description" {
-    default = "this security group is created for redis"  
+  default = "this security group is created for redis"  
 }
 
 variable "redis_vpn_ports" {
   default = [22, 6379]  
+}
+
+variable "redis_bastion_ports" {
+  default = [22, 6379]
 }
 
 variable "mysql_sg_name" {
@@ -73,11 +48,15 @@ variable "mysql_sg_name" {
 }
 
 variable "mysql_sg_description" {
-    default = "this security group is created for mysql"  
+  default = "this security group is created for mysql"  
 }
 
 variable "mysql_vpn_ports" {
   default = [22, 3306]  
+}
+
+variable "mysql_bastion_ports" {
+  default = [22, 3306]
 }
 
 variable "rabbitmq_sg_name" {
@@ -85,10 +64,14 @@ variable "rabbitmq_sg_name" {
 }
 
 variable "rabbitmq_sg_description" {
-    default = "this security group is created for rabbitmq"  
+  default = "this security group is created for rabbitmq"  
 }
 
 variable "rabbitmq_vpn_ports" {
+  default = [22, 5672]  
+}
+
+variable "rabbitmq_bastion_ports" {
   default = [22, 5672]  
 }
 
@@ -97,5 +80,85 @@ variable "catalogue_sg_name" {
 }
 
 variable "catalogue_sg_description" {
-    default = "this security group is created for catalogue"  
+  default = "this security group is created for catalogue"  
 }
+
+variable "user_sg_name" {
+  default = "user"
+}
+
+variable "user_sg_description" {
+  default = "this security group is created for user"  
+}
+
+variable "cart_sg_name" {
+  default = "cart"
+}
+
+variable "cart_sg_description" {
+  default = "this security group is created for cart"  
+}
+
+variable "shipping_sg_name" {
+  default = "shipping"
+}
+
+variable "shipping_sg_description" {
+  default = "this security group is created for shipping"  
+}
+
+variable "payment_sg_name" {
+  default = "payment"
+}
+
+variable "payment_sg_description" {
+  default = "this security group is created for payment"  
+}
+
+variable "backend-alb_sg_name" {
+  default = "backend-alb"
+}
+
+variable "backend-alb_sg_description" {
+  default = "this security group is created for backend-alb"  
+}
+
+variable "frontend_sg_name" {
+  default = "frontend"
+}
+
+variable "frontend_sg_description" {
+  default = "this security group is created for frontend"  
+}
+
+variable "frontend-alb_sg_name" {
+  default = "frontend-alb"
+}
+
+variable "frontend-alb_sg_description" {
+  default = "this security group is created for frontend-alb"  
+}
+
+variable "bastion_sg_name" {
+  default = "bastion"
+}
+
+variable "bastion_sg_description" {
+  default = "this security group is created for bastion"  
+}
+
+variable "vpn_sg_name" {
+  default = "vpn"
+}
+
+variable "vpn_sg_description" {
+  default = "this security group is created for vpn"  
+}
+
+
+
+
+
+
+
+
